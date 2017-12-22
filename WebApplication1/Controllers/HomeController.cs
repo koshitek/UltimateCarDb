138dea0c1c7e7677ad935ae5dcd14f07f1d34c76
Steven Soong <koshitek@hotmail.com>
2017-12-21 22:52:04 -0800

    Added code first for vehicles

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using WebApplication1.DatabaseContext;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -10,7 +12,18 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
+            AddVehicles();
             return View();
+        }
+
+        public void AddVehicles()
+        {
+            using (var db = new VehicleDBContext())
+            {
+                var vehicle = new Vehicle { Name = "MyfirstVehicle" };
+                db.Vehicles.Add(vehicle);
+                db.SaveChanges();
+            }
         }
     }
 }
---
+++ WebApplication1/DbContext/VehicleDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using WebApplication1.Models;
+
+namespace WebApplication1.DatabaseContext
+{
+    class VehicleDBContext : DbContext
+    {
+        public VehicleDBContext() : base("VehicleDB")
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<VehicleDBContext>());
+        }
+
+        public DbSet<Vehicle> Vehicles
+        {
+            get;
+            set;
+        }
+    }
+}
