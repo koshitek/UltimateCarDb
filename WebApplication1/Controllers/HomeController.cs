f1b6dc02ef23399aa00d315b53714202d766389e
koshitek <ssoong@internal.bcpharmacists.org>
2018-08-29 16:31:43 -0700

    Add years view

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -10,6 +10,10 @@ namespace WebApplication1.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        // api/years
+        // api/manufacturer
+        // api/vehicle/id
+
         public ActionResult Index()
         {
             using (var db = new VehicleDBContext())
@@ -18,6 +22,15 @@ namespace WebApplication1.Controllers
                 return View(manufacturers.ToList());
             }
         }
-
+        public ActionResult Years()
+        {
+            // get all years that have vehicles in it.
+            using (var db = new VehicleDBContext())
+            {
+                var vehiclesYears = db.Vehicles.Select(s => s.ProductionYear).Distinct().ToList();
+                var distinctYears = db.ProductionYears.Where(s => vehiclesYears.Contains(s.ProductionYearId));
+                return View(distinctYears.ToList());
+            }
+        }
     }
 }
